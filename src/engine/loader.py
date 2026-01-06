6b528a3b9a7016b5b1de83092bab8f90a8e3e0fb
Raphael <107597299+raphaelsette@users.noreply.github.com>
2026-01-06 01:11:17 +0000

    feat: definindo o splitter, chunk size e overlap

--- src/engine/loader.py
+++ src/engine/loader.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 docs = []
 
@@ -12,4 +13,8 @@ for n in Path('data/raw/').glob('*.pdf'):
     except Exception as e:
         print(f"Erro ao carregar o arquivo {n.name} - {e}")
 
-print(f"Total de documentos carregados: {len(docs)}")
+# print(f"Total de documentos carregados: {len(docs)}")
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=30)
+
+chunks = splitter.split_documents(docs)
